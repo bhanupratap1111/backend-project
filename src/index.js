72c1817b810af6cad9789ca32bcb0c2cca91166d
bhanupratap1111 <bhanupratap221514@gmail.com>
2025-02-13 02:42:50 +0530

    connection to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB();
+
+
+
+/*import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express";
+const app = express();
+
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error) => {
+            console.log("Error:", error)
+            throw error
+        })
+        
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    }
+    catch(err){
+        console.error("Error connecting to the database: ", err)
+        throw err
+    }
+})()
+
+function connectDB() {
+  mongoose.connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}*/
