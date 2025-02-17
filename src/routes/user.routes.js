e4802b8e7a604a6ea36d8b78950fc19f995235be
bhanupratap1111 <bhanupratap221514@gmail.com>
2025-02-18 04:21:48 +0530

    management features included

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,13 @@
 import {Router} from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    updateAccountDetails } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,5 +30,15 @@ router.route("/register").post(
 router.route("/login").post( loginUser )
 
 router.route("/logout").post(verifyJWT, logoutUser )
+
+router.route("/refresh-token").post( refreshAccessToken )
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
 
 export default router;
